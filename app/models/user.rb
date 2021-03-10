2149b99731db872895e36bdbd5701d2ad79de523
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-10 09:56:22 -0500

    adding validations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
+    validates :username, presence: true, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+
+    has_secure_password #this takes care of initial password validations but won't do everything for password confirmation field
+    
     has_many :teachers
     has_many :students
-
-    has_secure_password
 end
