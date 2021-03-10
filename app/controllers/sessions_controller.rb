c3dbf4df583bd22040560050b8c8f058e4162920
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-10 13:28:17 -0500

    adding full name helpers and building user show

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,6 +13,8 @@ class SessionsController < ApplicationController
             redirect_to user_path(@user)
         else
             redirect_to '/login'
+            #if you want to be less secure and have fields show the previous attempted info, 
+            #use the render.  You will then have to change the login page to form_tag or maybe form_with?
         end
     end
 
