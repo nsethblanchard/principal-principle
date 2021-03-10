920f247337d5bc1931aa5ec04bff512ff32cd200
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-10 09:10:47 -0500

    tweaked create action in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,7 +6,7 @@ class UsersController < ApplicationController
 
     def create
         @user = User.new(user_params)
-        if @user.valid?
+        if @user.save
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
@@ -15,12 +15,12 @@ class UsersController < ApplicationController
     end
 
     def show
-
+        @user = User.find(params[:id])
     end
 
     private
 
     def user_params
-        require(:user).permit(:username, :email, :password, :school)
+        params.require(:user).permit(:username, :email, :password, :school)
     end
 end
