656e0a7b4241cfa2121d7db80fce76946af0debd
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-10 09:29:44 -0500

    adding actions in sessions and determining flow

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   
+  root 'sessions#home'
   get '/signup' => "users#new"
   get '/login' => "sessions#new"
+  post '/login' => "sessions#create"
 
   resources :assignments
   resources :teachers
