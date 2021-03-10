a0759073dd2cc7588ba845a52b58ce4b2996c3ba
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-10 10:39:24 -0500

    stubbed out login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,11 +2,18 @@ class SessionsController < ApplicationController
 
 
     def new
-
+        @user = User.new
+        render :login
     end
 
     def create
-
+        @user = User.find_by(username: params[:user][:username])
+        if @user && @user.authenticate(params[:user][:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to '/login'
+        end
     end
 
     def home
