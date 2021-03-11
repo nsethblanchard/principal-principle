66052c69db30b61798db9e2f5f29803861e58c0d
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-11 10:06:40 -0500

    flash message for incorrect login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,6 +10,7 @@ class SessionsController < ApplicationController
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
+            flash[:message] = "You have entered incorrect email and/or password, please try again."
             redirect_to '/login'
             #if you want to be less secure and have fields show the previous attempted info, 
             #use the render.  You will then have to change the login page to form_tag or maybe form_with?
