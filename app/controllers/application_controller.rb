ed72a5c9fb079704fb83107d92c9a43e14cc5854
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-11 10:42:18 -0500

    notes for helpers and app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    #put methods in here you will use in your controllers primarily and use a lot
+    #if you are planning to just use them in views, write them in the appropriate helper file and then "include" them here
+
+    
+    #the 'helper_method' just gives permission for these methods to be used in the views
     helper_method :current_user, :logged_in?
 
 
@@ -11,4 +17,6 @@ class ApplicationController < ActionController::Base
     def logged_in?
         !!current_user
     end
+
+    #potentially add a redirect_if_not_logged_in type of method, then add to user actions
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,8 @@
 module ApplicationHelper
+    
+    #methods that goes in these files are going to be used primarily in views
+    #if you want to use them mainly in your controllers, put them in the application_controller file
+
     def full_name(person)
         person.first_name + " " + person.last_name
     end
