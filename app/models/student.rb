a85c86485cb092eb6ee04bf283c4a30b6d4e1498
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-12 10:47:39 -0500

    arbitrary commit for random changes

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,5 +1,7 @@
-class Student < ApplicationRecord
+class Student < ApplicationRecord 
   belongs_to :user
   has_many :assignments
   has_many :teachers, through: :assignments
+
+  #add validation for grade to limit 1-12?
 end
