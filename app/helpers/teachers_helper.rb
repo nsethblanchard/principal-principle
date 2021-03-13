c6bfab1de7ad64044b118eaf1d46c1013645bda2
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-13 11:15:37 -0500

    corrected each for students/assignments in teacher show

--- app/helpers/teachers_helper.rb
+++ app/helpers/teachers_helper.rb
@@ -1,4 +1,13 @@
 module TeachersHelper
 
-    
+
+    #Was going to create a method for hash creation for list of uniq students and their assignments...
+
+    # def students_and_their_assignments(teacher)
+    #     stud_ass = Hash.new()
+    #     teacher.students.each do |student|
+    #         stud_ass = Hash.new({full_name(student): student.})
+    #     end
+
+    # end
 end
