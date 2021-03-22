241a23a1a4566ae3fdb2f82b1ea746f1a633aec9
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-22 08:24:06 -0400

    added full name helper method to other views for assignments

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord 
   belongs_to :user
-  has_many :assignments
-  has_many :teachers, through: :assignments
+  has_many :assignments, :dependent => :destroy
+  has_many :teachers, through: :assignments, :dependent => :destroy
 
   #add validation for grade to limit 1-12?
   validates :first_name, :last_name, :fav_movie, presence: true
--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,7 +1,12 @@
 class Teacher < ApplicationRecord
   belongs_to :user
-  has_many :assignments
-  has_many :students, through: :assignments
+  has_many :assignments, :dependent => :destroy
+  has_many :students, through: :assignments, :dependent => :destroy
 
   validates :first_name, :last_name, :subject, :hobby, presence: true
+
+  def full_name
+    fn = self.first_name + " " + self.last_name
+    fn.titleize
+  end
 end
