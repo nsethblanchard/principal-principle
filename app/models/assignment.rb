3355089fc75bd8b6d8491d651a2ac92c86498db1
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-24 08:20:35 -0400

    changed class method to true scope method in assignment

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -8,15 +8,17 @@ class Assignment < ApplicationRecord
   scope :alpha, -> { order (:content) }
 
     #this is the scope method = usually called in the controller
-
+    
     # same as def alpha
-
     # end
 
-  def self.overdue_assignments
-    Assignment.where('due_date < ? and completed == false', DateTime.now).order(:desc)    #and not completed!!!
-  end
+  # def self.overdue_assignments
+  #   Assignment.where('due_date < ? and completed == false', DateTime.now).order(:desc)    
+  # end
 
+  scope :overdue_assignments, -> (date = DateTime.now) { where("due_date < ?", date) && where("completed == false") }
+  
+  
   # def honor_roll ?????
 
   
