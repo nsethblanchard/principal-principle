ffb8e429e809a3f69243d2a885b8a3f927ab8bc9
Seth Blanchard <nsethblanchard@gmail.com>
2021-03-25 09:48:13 -0400

    minor fixes to if else

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -5,19 +5,14 @@ class Assignment < ApplicationRecord
   validates :title, :content, :due_date, presence: true
 
   
-  scope :alpha, -> { order (:content) }
+  scope :alpha, -> { order (:content) }  
 
-    #this is the scope method = usually called in the controller
-    
-    # same as def alpha
-    # end
+  scope :overdue_assignments, -> (date = DateTime.now) { where("due_date < ?", date) && where("completed == false") }
+  #this is the scope method = usually called in the controller
 
   # def self.overdue_assignments
   #   Assignment.where('due_date < ? and completed == false', DateTime.now).order(:desc)    
   # end
-
-  scope :overdue_assignments, -> (date = DateTime.now) { where("due_date < ?", date) && where("completed == false") }
-  
   
   # def honor_roll ?????
 
